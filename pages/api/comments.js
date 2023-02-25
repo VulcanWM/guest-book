77188fe2db01291ddfb06ff5a1e04cd484948b6f
Medea <logmedha09@gmail.com>
2023-02-25 21:40:31 +0000

    added commenting

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -7,9 +7,11 @@ export default async function handler(req, res) {
   const db = client.db("comments");
   switch (req.method) {
     case "POST":
-      let bodyObject = JSON.parse(req.body);
+      console.log(req.body)
+      let bodyObject = JSON.parse(JSON.stringify(req.body));
+      console.log(bodyObject)
       let myComment = await db.collection("comments").insertOne(bodyObject);
-      res.json(myComment.ops[0]);
+      res.redirect("/")
       break;
     case "GET":
       const allComments = await db.collection("comments").find({}).toArray();
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,12 @@ export default function HomePage({ allComments }) {
     <div>
       <Layout pageTitle="Home">
         <h1>Hello</h1>
+        <form action='/api/comments' method='POST'>
+          <h2>Comment</h2>
+          <input name="User" id="title"></input>
+          <textarea name="Body"></textarea>
+          <button type="submit">Button</button>
+        </form>
         {
           allComments['data'].map((key, index) => ( 
             <p key={key['_id']}>{key['User']}: {key['Body']}</p> 
