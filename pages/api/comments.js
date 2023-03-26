1da585bfebe97520f13e7a1afa2a50ebea5c2a4d
VulcanWM <logmedha09@gmail.com>
2023-03-26 19:32:23 +0100

    trying to fix 504 bug again

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -5,51 +5,47 @@ import { authOptions } from './/auth/[...nextauth]'
 import { getServerSession } from "next-auth/next"
 
 export default async function handler(req, res) {
-  const session = await getServerSession(req, res, authOptions)
-  if (!session) {
-    res.redirect("/?msg=You have to be logged in to comment");
-    return;
-  }
-  console.log(session)
-  console.log(session.user)
-  console.log(session.user.image)
   const client = await clientPromise;
   const db = client.db("comments");
-  switch (req.method) {
-    case "POST":
-      const currentDate = new Date().toUTCString();
-      const cookies = new Cookies(req, res)
-      if (cookies.get('timeset') == null){
-        // no cookie set
-      } else {
-        var datenow = new Date(Date.parse(currentDate));
-        var datethen = new Date(Date.parse(cookies.get('timeset')));
-        var hours = Math.abs(datenow - datethen) / 36e5;
-        if (hours < 1){
-          res.redirect("/?msg=You can only message once an hour")
-          break;
-        }
+  console.log(req.method)
+  if (req.method == "GET"){
+    const allComments = await db.collection("comments").find({}).toArray();
+    allComments.reverse()
+    res.json({ status: 200, data: allComments });
+  } else if (req.method == 'POST') {
+    const session = await getServerSession(req, res, authOptions)
+    if (!session) {
+      res.redirect("/?msg=You have to be logged in to comment");
+    }
+    console.log(session)
+    console.log(session.user)
+    console.log(session.user.image)
+    const currentDate = new Date().toUTCString();
+    const cookies = new Cookies(req, res)
+    if (cookies.get('timeset') == null){
+      // no cookie set
+    } else {
+      var datenow = new Date(Date.parse(currentDate));
+      var datethen = new Date(Date.parse(cookies.get('timeset')));
+      var hours = Math.abs(datenow - datethen) / 36e5;
+      if (hours < 1){
+        res.redirect("/?msg=You can only message once an hour")
       }
-      req.body['Created'] = currentDate
-      var cleaned = censorjs.clean(req.body['Body']);
-      req.body['Body'] = cleaned
-      if (req.body['User'].replaceAll(" ", "") == ""){
-        res.redirect("/")
-        if (req.body['Body'].replaceAll(" ", "") == ""){
-          res.redirect("/")
-        }
-      }
-      let bodyObject = JSON.parse(JSON.stringify(req.body));
-      await db.collection("comments").insertOne(bodyObject);
-      cookies.set('timeset', currentDate, {
-        httpOnly: true
-      })
+    }
+    req.body['Created'] = currentDate
+    var cleaned = censorjs.clean(req.body['Body']);
+    req.body['Body'] = cleaned
+    if (req.body['User'].replaceAll(" ", "") == ""){
       res.redirect("/")
-      break;
-    case "GET":
-      const allComments = await db.collection("comments").find({}).toArray();
-      allComments.reverse()
-      res.json({ status: 200, data: allComments });
-      break;
+      if (req.body['Body'].replaceAll(" ", "") == ""){
+        res.redirect("/")
+      }
+    }
+    let bodyObject = JSON.parse(JSON.stringify(req.body));
+    await db.collection("comments").insertOne(bodyObject);
+    cookies.set('timeset', currentDate, {
+      httpOnly: true
+    })
+    res.redirect("/")
   }
 }
