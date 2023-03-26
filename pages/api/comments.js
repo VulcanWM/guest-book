9d60e677356726dbd3fbda3536d013154fac35fe
MadhuSaini22 <madhusaini46810@gmail.com>
2023-03-26 13:33:58 +0530

    added github OAuth

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,46 +1,52 @@
-var censorjs = require('censorjs');
-import Cookies from 'cookies'
+var censorjs = require("censorjs");
+import Cookies from "cookies";
+import { getSession } from "next-auth/react";
 
 import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
+  const session = await getSession({ req });
   const client = await clientPromise;
-  const db = client.db("comments");
-  switch (req.method) {
-    case "POST":
-      const currentDate = new Date().toUTCString();
-      const cookies = new Cookies(req, res)
-      if (cookies.get('timeset') == null){
-        // no cookie set
-      } else {
-        var datenow = new Date(Date.parse(currentDate));
-        var datethen = new Date(Date.parse(cookies.get('timeset')));
-        var hours = Math.abs(datenow - datethen) / 36e5;
-        if (hours < 1){
-          res.redirect("/?msg=You can only message once an hour")
-          break;
+  if (!session) {
+    res.status(401).json({ error: "Permission Denied!!" });
+  } else {
+    const db = client.db("comments");
+    switch (req.method) {
+      case "POST":
+        const currentDate = new Date().toUTCString();
+        const cookies = new Cookies(req, res);
+        if (cookies.get("timeset") == null) {
+          // no cookie set
+        } else {
+          var datenow = new Date(Date.parse(currentDate));
+          var datethen = new Date(Date.parse(cookies.get("timeset")));
+          var hours = Math.abs(datenow - datethen) / 36e5;
+          if (hours < 1) {
+            res.redirect("/?msg=You can only message once an hour");
+            break;
+          }
         }
-      }
-      req.body['Created'] = currentDate
-      var cleaned = censorjs.clean(req.body['Body']);
-      req.body['Body'] = cleaned
-      if (req.body['User'].replaceAll(" ", "") == ""){
-        res.redirect("/")
-        if (req.body['Body'].replaceAll(" ", "") == ""){
-          res.redirect("/")
+        req.body["Created"] = currentDate;
+        var cleaned = censorjs.clean(req.body["Body"]);
+        req.body["Body"] = cleaned;
+        if (req.body["User"].replaceAll(" ", "") == "") {
+          res.redirect("/");
+          if (req.body["Body"].replaceAll(" ", "") == "") {
+            res.redirect("/");
+          }
         }
-      }
-      let bodyObject = JSON.parse(JSON.stringify(req.body));
-      await db.collection("comments").insertOne(bodyObject);
-      cookies.set('timeset', currentDate, {
-        httpOnly: true
-      })
-      res.redirect("/")
-      break;
-    case "GET":
-      const allComments = await db.collection("comments").find({}).toArray();
-      allComments.reverse()
-      res.json({ status: 200, data: allComments });
-      break;
+        let bodyObject = JSON.parse(JSON.stringify(req.body));
+        await db.collection("comments").insertOne(bodyObject);
+        cookies.set("timeset", currentDate, {
+          httpOnly: true,
+        });
+        res.redirect("/");
+        break;
+      case "GET":
+        const allComments = await db.collection("comments").find({}).toArray();
+        allComments.reverse();
+        res.json({ status: 200, data: allComments });
+        break;
+    }
   }
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,36 +1,60 @@
-import Layout from '../components/layout'
-import styles from '../styles/home.module.css'
-import { useRouter } from 'next/router'
-
+import Layout from "../components/layout";
+import styles from "../styles/home.module.css";
+import { useRouter } from "next/router";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function HomePage({ allComments }) {
-  const router = useRouter()
-  const { msg } = router.query
+  const { data: session } = useSession();
+  const router = useRouter();
+  const { msg } = router.query;
   return (
     <Layout pageTitle="Home">
-      <h1>VulcanWM's GuestBook</h1>
-      {msg ?
-        <h3 className={styles.red}>{msg}</h3>
-      :
-        <h3 className={styles.lightfont}>Say hello</h3>
-      }
-      <form action='/api/comments' method='POST'>
-        <input name="User" id="title" required></input>
-        <br/>
-        <label htmlFor="Body">Message</label>
-        <br/>
-        <input className={styles.message} name="Body" required></input>
-        <button className={styles.send} type="submit">Send</button>
-      </form>
-      <div id="comments" className={styles.comments}>
-        {
-          allComments['data'].map((key, index) => (
-            <div id={index} className={styles.comment}>
-              <p><strong>{key['User']}</strong> at <span className={styles.lightfont}>{key['Created']}</span></p> 
-              <p>{key['Body']}</p>
+      <div className={styles.main}>
+       {/* check if user is logged in or not */}
+        {!session && (
+          // don't have session
+          <button className={styles.button} onClick={() => signIn()}>
+            Sign In with Github
+          </button>
+        )}
+        {session && (
+          // session found
+          <div>
+            <p>Hello, {session?.user?.name}</p>
+
+            <button className={styles.button} onClick={() => signOut()}>
+              Sign Out
+            </button>
+
+            <h1>VulcanWM's GuestBook</h1>
+            {msg ? (
+              <h3 className={styles.red}>{msg}</h3>
+            ) : (
+              <h3 className={styles.lightfont}>Say hello</h3>
+            )}
+            <form action="/api/comments" method="POST">
+              <input name="User" id="title" required></input>
+              <br />
+              <label htmlFor="Body">Message</label>
+              <br />
+              <input className={styles.message} name="Body" required></input>
+              <button className={styles.send} type="submit">
+                Send
+              </button>
+            </form>
+            <div id="comments" className={styles.comments}>
+              {allComments["data"].map((key, index) => (
+                <div id={index} className={styles.comment}>
+                  <p>
+                    <strong>{key["User"]}</strong> at{" "}
+                    <span className={styles.lightfont}>{key["Created"]}</span>
+                  </p>
+                  <p>{key["Body"]}</p>
+                </div>
+              ))}
             </div>
-          ))
-        }
+          </div>
+        )}
       </div>
     </Layout>
   );
